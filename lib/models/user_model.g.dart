6019270c236eefc9afbd28db5d5bd3a8912fb268
krcgk <karaca.gkhan@gmail.com>
2023-06-03 13:22:28 +0300

    added mobx and get it structure

--- lib/models/user_model.g.dart
+++ lib/models/user_model.g.dart
@@ -10,16 +10,10 @@ UserModel _$UserModelFromJson(Map<String, dynamic> json) => UserModel(
       firstName: json['firstName'] as String,
       lastName: json['lastName'] as String,
       phoneNumber: json['phoneNumber'] as String,
-      firstName1: json['firstName1'] as String,
-      lastName2: json['lastName2'] as String,
-      phoneNumber3: json['phoneNumber3'] as String,
     );
 
 Map<String, dynamic> _$UserModelToJson(UserModel instance) => <String, dynamic>{
       'firstName': instance.firstName,
       'lastName': instance.lastName,
       'phoneNumber': instance.phoneNumber,
-      'firstName1': instance.firstName1,
-      'lastName2': instance.lastName2,
-      'phoneNumber3': instance.phoneNumber3,
     };
